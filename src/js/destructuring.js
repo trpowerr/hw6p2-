8d6c6dab69133702bc2b36cf4fe3bdeb401abc6d
trpowerr <mixazbubl@yandex.ru>
2020-02-12 16:52:26 +0300

    description deafault

--- src/js/destructuring.js
+++ src/js/destructuring.js
@@ -24,7 +24,7 @@ const data = {
   function dataDestructuring () {
       for (let props in data) {
         if (props == 'special') {
-          const {special: [{id, name, icon, description}]} = data;  
+          const {special: [{id, name, icon, description = 'Засекречено'}]} = data;  
           // console.log(id, name, icon, description);
           return {id, name, icon, description};
         }
